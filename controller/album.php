a81506ae494ae902983df47926bfce830212406f
lRaffou <akira182@proton.me>
2025-04-09 22:51:39 +0200

    actualisation albums page

--- controller/album.php
+++ controller/album.php
@@ -2,13 +2,15 @@
 
 require_once '../view/viewHeader.php';
 require_once '../view/viewAlbum.php';
+require_once '../view/viewFooter.php';
 
 function renderAlbumPage()
 {
     $header = renderHeader();
     $content = renderAlbum();
+    $footer = renderFooter();
 
-    echo $header . $content;
+    echo $header . $content . $footer;
 }
 
 renderAlbumPage();
--- view/viewAlbum.php
+++ view/viewAlbum.php
@@ -5,62 +5,106 @@ function renderAlbum()
     ob_start();
     ?>
     <main class="mainAlbum">
-        <section class="sectionAlbum">
+        <section class="albumHero">
             <h1>Nos Albums</h1>
-            <div class="containerAlbum">
-                <article class="albumCard">
+            <p>Découvrez nos dernières sorties et notre catalogue musical</p>
+        </section>
+
+        <section class="featuredAlbums">
+            <h2>Albums à la Une</h2>
+            <div class="albumContainer">
+                <article class="albumCard featured">
                     <div class="albumImage">
                         <img src="/img/albums/album1.jpg" alt="Album Red Rave">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/intl-fr/album/3qFHJrWeitkjPo1OWmRBHa" class="btnListen"
+                                target="_blank">
+                                <i class="fab fa-spotify"></i>
+                                Écouter sur Spotify
+                            </a>
+                        </div>
                     </div>
                     <div class="albumInfo">
-                        <h2>Red Rave</h2>
-                        <p>Artiste: Dao Brothers</p>
-                        <p>Date de sortie: 01/01/2023</p>
-                        <a href="#" class="btnEcouter">Écouter</a>
+                        <h3>Red Rave</h3>
+                        <p class="artistName">Artiste: Dao Brothers</p>
+                        <p class="releaseDate">Date de sortie: 01/01/2023</p>
+                        <div class="albumTags">
+                            <span class="tag">Bass House</span>
+                            <span class="tag">Techno</span>
+                        </div>
                     </div>
                 </article>
+            </div>
+        </section>
 
+        <section class="albumCatalog">
+            <h2>Notre Catalogue</h2>
+            <div class="albumGrid">
                 <article class="albumCard">
                     <div class="albumImage">
                         <img src="/img/albums/album2.jpg" alt="Album Imagination">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/intl-fr/album/0IvMcp0vjpPsioa70rJ0rB" class="btnListen"
+                                target="_blank">
+                                <i class="fab fa-spotify"></i>
+                                Écouter sur Spotify
+                            </a>
+                        </div>
                     </div>
                     <div class="albumInfo">
-                        <h2>Imagination</h2>
-                        <p>Artiste: Fernando</p>
-                        <p>Date de sortie: 15/03/2023</p>
-                        <a href="#" class="btnEcouter">Écouter</a>
+                        <h3>Imagination</h3>
+                        <p class="artistName">Artiste: Fernando</p>
+                        <p class="releaseDate">Date de sortie: 15/03/2023</p>
+                        <div class="albumTags">
+                            <span class="tag">Techno</span>
+                        </div>
                     </div>
                 </article>
 
                 <article class="albumCard">
                     <div class="albumImage">
                         <img src="/img/albums/album3.jpg" alt="Album DJ BABE Event">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/intl-fr/track/0Jc3ODaHZsJBSvSwQx4Qwm" class="btnListen"
+                                target="_blank">
+                                <i class="fab fa-spotify"></i>
+                                Écouter sur Spotify
+                            </a>
+                        </div>
                     </div>
                     <div class="albumInfo">
-                        <h2>DJ BABE Event</h2>
-                        <p>Artiste: DJ BABE</p>
-                        <p>Date de sortie: 30/06/2023</p>
-                        <a href="#" class="btnEcouter">Écouter</a>
+                        <h3>DJ BABE Event</h3>
+                        <p class="artistName">Artiste: DJ BABE</p>
+                        <p class="releaseDate">Date de sortie: 30/06/2023</p>
+                        <div class="albumTags">
+                            <span class="tag">Tech House</span>
+                        </div>
                     </div>
                 </article>
 
                 <article class="albumCard">
                     <div class="albumImage">
                         <img src="/img/albums/album4.jpg" alt="Album Feel Good">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/intl-fr/track/0Jc3ODaHZsJBSvSwQx4Qwm" class="btnListen"
+                                target="_blank">
+                                <i class="fab fa-spotify"></i>
+                                Écouter sur Spotify
+                            </a>
+                        </div>
                     </div>
                     <div class="albumInfo">
-                        <h2>Feel Good</h2>
-                        <p>Artiste: DJ BABE</p>
-                        <p>Date de sortie: 10/09/2023</p>
-                        <a href="#" class="btnEcouter">Écouter</a>
+                        <h3>Feel Good</h3>
+                        <p class="artistName">Artiste: DJ BABE</p>
+                        <p class="releaseDate">Date de sortie: 10/09/2023</p>
+                        <div class="albumTags">
+                            <span class="tag">Tech House</span>
+                        </div>
                     </div>
                 </article>
             </div>
         </section>
     </main>
-    <footer>
-        <p>&copy; 2023 Dao Family Records. Tous droits réservés.</p>
-    </footer>
     <?php
     return ob_get_clean();
 }
