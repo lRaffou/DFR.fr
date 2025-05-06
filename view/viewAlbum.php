d7df7027fed2d73ac22a316052ecbc0e9c36c7de
lRaffou <akira182@proton.me>
2025-05-06 21:26:12 +0200

    update album page

--- view/viewAlbum.php
+++ view/viewAlbum.php
@@ -15,18 +15,18 @@ function renderAlbum()
             <div class="albumContainer">
                 <article class="albumCard featured">
                     <div class="albumImage">
-                        <img src="/img/albums/album1.jpg" alt="Album Red Rave">
+                        <img src="/img/albums/album1.png" alt="Album - How Can I">
                         <div class="albumOverlay">
-                            <a href="https://open.spotify.com/intl-fr/album/3qFHJrWeitkjPo1OWmRBHa" class="btnListen"
-                                target="_blank">
+                            <a href="https://open.spotify.com/intl-fr/track/221oa36Tj0G8rSX8nbuoyf?si=d53c99e5430a4977"
+                                class="btnListen" target="_blank">
                                 <i class="fab fa-spotify"></i> Écouter sur Spotify
                             </a>
                         </div>
                     </div>
                     <div class="albumInfo">
-                        <h3>Red Rave</h3>
-                        <p class="artistName">Artiste : Dao Brothers</p>
-                        <p class="releaseDate">Date de sortie : 01/01/2023</p>
+                        <h3>How Can I</h3>
+                        <p class="artistName">Artiste : Dj Babe</p>
+                        <p class="releaseDate">Date de sortie : 01/12/2024</p>
                         <div class="albumTags">
                             <span class="tag">Bass House</span>
                             <span class="tag">Techno</span>
@@ -42,7 +42,7 @@ function renderAlbum()
 
                 <article class="albumCard">
                     <div class="albumImage">
-                        <img src="/img/albums/album2.jpg" alt="Album Imagination">
+                        <img src="/img/albums/album2.jpg" alt="Album - Imagination">
                         <div class="albumOverlay">
                             <a href="https://open.spotify.com/intl-fr/album/0IvMcp0vjpPsioa70rJ0rB" class="btnListen"
                                 target="_blank">
@@ -52,8 +52,8 @@ function renderAlbum()
                     </div>
                     <div class="albumInfo">
                         <h3>Imagination</h3>
-                        <p class="artistName">Artiste : Fernando</p>
-                        <p class="releaseDate">Date de sortie : 15/03/2023</p>
+                        <p class="artistName">Artiste : Fernando Gomez</p>
+                        <p class="releaseDate">Date de sortie : 15/09/2024</p>
                         <div class="albumTags">
                             <span class="tag">Techno</span>
                         </div>
@@ -62,18 +62,18 @@ function renderAlbum()
 
                 <article class="albumCard">
                     <div class="albumImage">
-                        <img src="/img/albums/album3.jpg" alt="Album DJ BABE Event">
+                        <img src="/img/albums/album3.jpg" alt="Album - Red Night">
                         <div class="albumOverlay">
-                            <a href="https://open.spotify.com/intl-fr/track/0Jc3ODaHZsJBSvSwQx4Qwm" class="btnListen"
-                                target="_blank">
+                            <a href="https://open.spotify.com/album/7nDOtP7tieus0W13J4RCho?si=di0B3QJ2R4Sg6jj_9HFMnQ"
+                                class="btnListen" target="_blank">
                                 <i class="fab fa-spotify"></i> Écouter sur Spotify
                             </a>
                         </div>
                     </div>
                     <div class="albumInfo">
-                        <h3>DJ BABE Event</h3>
-                        <p class="artistName">Artiste : DJ BABE</p>
-                        <p class="releaseDate">Date de sortie : 30/06/2023</p>
+                        <h3>Red Night</h3>
+                        <p class="artistName">Artiste : Red Marshal</p>
+                        <p class="releaseDate">Date de sortie : 30/06/2022</p>
                         <div class="albumTags">
                             <span class="tag">Tech House</span>
                         </div>
@@ -82,18 +82,78 @@ function renderAlbum()
 
                 <article class="albumCard">
                     <div class="albumImage">
-                        <img src="/img/albums/album4.jpg" alt="Album Feel Good">
+                        <img src="/img/albums/album4.jpg" alt="Album - Feel Good">
                         <div class="albumOverlay">
-                            <a href="https://open.spotify.com/intl-fr/track/0Jc3ODaHZsJBSvSwQx4Qwm" class="btnListen"
-                                target="_blank">
+                            <a href="https://open.spotify.com/intl-fr/track/0Jc3ODaHZsJBSvSwQx4Qwm?si=91bd0417088d4053"
+                                class="btnListen" target="_blank">
                                 <i class="fab fa-spotify"></i> Écouter sur Spotify
                             </a>
                         </div>
                     </div>
                     <div class="albumInfo">
                         <h3>Feel Good</h3>
-                        <p class="artistName">Artiste : DJ BABE</p>
-                        <p class="releaseDate">Date de sortie : 10/09/2023</p>
+                        <p class="artistName">Artiste : Dj Babe</p>
+                        <p class="releaseDate">Date de sortie : 10/09/2024</p>
+                        <div class="albumTags">
+                            <span class="tag">Tech House</span>
+                        </div>
+                    </div>
+                </article>
+
+                <article class="albumCard">
+                    <div class="albumImage">
+                        <img src="/img/albums/album5.png" alt="Album - Acid Conversation">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/album/6NCGuVlBWQ3LR0McsRcQ4K?si=u5ghKA_iSj2Kn_HN7E_CTA"
+                                class="btnListen" target="_blank">
+                                <i class="fab fa-spotify"></i> Écouter sur Spotify
+                            </a>
+                        </div>
+                    </div>
+                    <div class="albumInfo">
+                        <h3>Acid Conversation</h3>
+                        <p class="artistName">Artiste : Dao Brothers</p>
+                        <p class="releaseDate">Date de sortie : 11/08/2024</p>
+                        <div class="albumTags">
+                            <span class="tag">Tech House</span>
+                        </div>
+                    </div>
+                </article>
+
+                <article class="albumCard">
+                    <div class="albumImage">
+                        <img src="/img/albums/album6.png" alt="Album - Darkness">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/intl-fr/track/0GvGAPCMYmjJVEOZquBC7U?si=99bc00c43d5645e6"
+                                class="btnListen" target="_blank">
+                                <i class="fab fa-spotify"></i> Écouter sur Spotify
+                            </a>
+                        </div>
+                    </div>
+                    <div class="albumInfo">
+                        <h3>Darkness</h3>
+                        <p class="artistName">Artiste : Fernando Gomez</p>
+                        <p class="releaseDate">Date de sortie : 11/05/2024</p>
+                        <div class="albumTags">
+                            <span class="tag">Tech House</span>
+                        </div>
+                    </div>
+                </article>
+
+                <article class="albumCard">
+                    <div class="albumImage">
+                        <img src="/img/albums/album7.png" alt="Album - My Eyes">
+                        <div class="albumOverlay">
+                            <a href="https://open.spotify.com/album/2djAiLCzmUyuqmJ0Fb9Dud?si=26AH8BqXR4eTunfQdZvvXA"
+                                class="btnListen" target="_blank">
+                                <i class="fab fa-spotify"></i> Écouter sur Spotify
+                            </a>
+                        </div>
+                    </div>
+                    <div class="albumInfo">
+                        <h3>My Eyes</h3>
+                        <p class="artistName">Artiste : Red Marshal</p>
+                        <p class="releaseDate">Date de sortie : 21/10/2021</p>
                         <div class="albumTags">
                             <span class="tag">Tech House</span>
                         </div>
