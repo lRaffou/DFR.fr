8ab3c9d3de9fb000dc5b406f60e139149ab8acdc
lRaffou <akira182@proton.me>
2025-05-06 18:43:23 +0200

    final design page event

--- view/viewEvenement.php
+++ view/viewEvenement.php
@@ -13,117 +13,65 @@ function renderEvenement()
         <section class="featuredEvent">
             <h2>Prochain Événement</h2>
             <div class="eventContainer">
+                <?php
+                $featuredEvents = [
+                    [
+                        'image' => '/img/nextEvent.jpg',
+                        'alt' => 'Coeur Meurtri - Open Air',
+                        'day' => '10-11',
+                        'month' => 'MAI',
+                        'title' => '🔴 Coeur Meurtri - Open Air 🔴',
+                        'tags' => ['Bass House', 'Techno', 'Hard Techno'],
+                        'description' => '"Cœur Meurtri by DJ Babe" est un événement musical 100% féminin au Port de l\'Embouchure à Toulouse.',
+                        'details' => 'Deux jours de performances exclusives pour célébrer les talents féminins dans l\'électronique !',
+                        'links' => [
+                            ['url' => 'https://fb.me/e/7OXhf42dF', 'text' => 'Plus d\'infos', 'class' => 'btnEvent'],
+                            ['url' => 'https://shotgun.live/fr/events/after-officiel-coeur-meurtri-open-air-2025', 'text' => 'Réserver', 'class' => 'btnEvent btnPrimary']
+                        ]
+                    ],
+                    [
+                        'image' => '/img/nextEvent2.jpg',
+                        'alt' => 'The Red Night Off - Édition #5',
+                        'day' => '16',
+                        'month' => 'MAI',
+                        'title' => '🔴 The Red Night Off - Édition #5 🔴',
+                        'tags' => ['Bass House', 'Techno', 'Hard Techno'],
+                        'description' => 'Retrouvons-nous au Full House pour la 5e édition de la Red Night avec NIVK, Red Marshal et Fernando Gomez !',
+                        'details' => 'Nuit rouge passion de 1H à 7H au Full House Toulouse.',
+                        'links' => [
+                            ['url' => 'https://www.instagram.com/p/DIvfIeyICU5/', 'text' => 'Plus d\'infos', 'class' => 'btnEvent'],
+                            ['url' => 'https://shotgun.live/fr/events/the-red-night-off-edition-5', 'text' => 'Réserver', 'class' => 'btnEvent btnPrimary']
+                        ]
+                    ]
+                ];
 
-                <article class="eventCard featured">
-                    <div class="eventImage">
-                        <img src="/img/nextEvent.jpg" alt="Coeur Meurtri - Open Air">
-                        <div class="eventDate">
-                            <span class="day">10-11</span>
-                            <span class="month">MAI</span>
+                foreach ($featuredEvents as $event): ?>
+                    <article class="eventCard featured">
+                        <div class="eventImage">
+                            <img src="<?= $event['image'] ?>" alt="<?= $event['alt'] ?>">
+                            <div class="eventDate">
+                                <span class="day"><?= $event['day'] ?></span>
+                                <span class="month"><?= $event['month'] ?></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="eventInfo">
-                        <h3>🔴 Coeur Meurtri - Open Air 🔴</h3>
-                        <div class="eventTags">
-                            <span class="tag">Bass House</span>
-                            <span class="tag">Techno</span>
-                            <span class="tag">Hard Techno</span>
+                        <div class="eventInfo">
+                            <h3><?= $event['title'] ?></h3>
+                            <div class="eventTags">
+                                <?php foreach ($event['tags'] as $tag): ?>
+                                    <span class="tag"><?= $tag ?></span>
+                                <?php endforeach; ?>
+                            </div>
+                            <p class="eventDescription"><?= $event['description'] ?></p>
+                            <p class="eventDetails"><?= $event['details'] ?></p>
+                            <div class="eventActions">
+                                <?php foreach ($event['links'] as $link): ?>
+                                    <a href="<?= $link['url'] ?>" target="_blank"
+                                        class="<?= $link['class'] ?>"><?= $link['text'] ?></a>
+                                <?php endforeach; ?>
+                            </div>
                         </div>
-                        <p class="eventDescription">
-                            "Cœur Meurtri by DJ Babe" est un événement musical 100% féminin au Port de l'Embouchure à
-                            Toulouse.
-                        </p>
-                        <p class="eventDetails">
-                            Deux jours de performances exclusives pour célébrer les talents féminins dans l'électronique !
-                        </p>
-                        <div class="eventActions">
-                            <a href="https://fb.me/e/7OXhf42dF" target="_blank" class="btnEvent">Plus d'infos</a>
-                            <a href="https://shotgun.live/fr/events/after-officiel-coeur-meurtri-open-air-2025"
-                                target="_blank" class="btnEvent btnPrimary">Réserver</a>
-                        </div>
-                    </div>
-                </article>
-
-                <article class="eventCard featured">
-                    <div class="eventImage">
-                        <img src="/img/nextEvent2.jpg" alt="The Red Night Off - Édition #5">
-                        <div class="eventDate">
-                            <span class="day">16</span>
-                            <span class="month">MAI</span>
-                        </div>
-                    </div>
-                    <div class="eventInfo">
-                        <h3>🔴 The Red Night Off - Édition #5 🔴</h3>
-                        <div class="eventTags">
-                            <span class="tag">Bass House</span>
-                            <span class="tag">Techno</span>
-                            <span class="tag">Hard Techno</span>
-                        </div>
-                        <p class="eventDescription">
-                            Retrouvons-nous au Full House pour la 5e édition de la Red Night avec NIVK, Red Marshal et
-                            Fernando Gomez !
-                        </p>
-                        <p class="eventDetails">
-                            Nuit rouge passion de 1H à 7H au Full House Toulouse.
-                        </p>
-                        <div class="eventActions">
-                            <a href="https://www.instagram.com/p/DIvfIeyICU5/" target="_blank" class="btnEvent">Plus
-                                d'infos</a>
-                            <a href="https://shotgun.live/fr/events/the-red-night-off-edition-5" target="_blank"
-                                class="btnEvent btnPrimary">Réserver</a>
-                        </div>
-                    </div>
-                </article>
-
-            </div>
-        </section>
-
-        <section class="pastEvents">
-            <h2>Événements Passés</h2>
-            <div class="pastEventGrid">
-
-                <article class="pastEventCard">
-                    <div class="pastEventImage">
-                        <img src="/img/Red_Night.png" alt="The Red Night - Open Air">
-                    </div>
-                    <div class="pastEventInfo">
-                        <h3>The Red Night - Open Air</h3>
-                        <p>Une soirée mémorable sous les étoiles</p>
-                        <div class="pastEventAction">
-                            <a href="https://www.instagram.com/p/C9C2SH5oDPu/" target="_blank" class="pastEventButton">Voir
-                                les photos</a>
-                        </div>
-                    </div>
-                </article>
-
-                <article class="pastEventCard">
-                    <div class="pastEventImage">
-                        <img src="/img/Red_Night.png" alt="Red Rave by Red Marshal">
-                    </div>
-                    <div class="pastEventInfo">
-                        <h3>Red Rave by Red Marshal</h3>
-                        <p>Une nuit de techno intense</p>
-                        <div class="pastEventAction">
-                            <a href="https://www.instagram.com/p/Cy2z4sLo6Lh/" target="_blank" class="pastEventButton">Voir
-                                les photos</a>
-                        </div>
-                    </div>
-                </article>
-
-                <article class="pastEventCard">
-                    <div class="pastEventImage">
-                        <img src="/img/Red_Night.png" alt="Red Rave Special Edition">
-                    </div>
-                    <div class="pastEventInfo">
-                        <h3>Red Rave Special Edition</h3>
-                        <p>Une édition spéciale inoubliable</p>
-                        <div class="pastEventAction">
-                            <a href="https://www.instagram.com/p/CyX3byPoxY5/" target="_blank" class="pastEventButton">Voir
-                                les photos</a>
-                        </div>
-                    </div>
-                </article>
-
+                    </article>
+                <?php endforeach; ?>
             </div>
         </section>
 
